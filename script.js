ed4d6cdedf6ad989233778ea3cf38c1260568889
simonpetersonline@gmail.com <simonpetersonline@gmail.com>
2023-11-11 17:35:57 +0000

    first working calculation, using 7,8,9,., and plus

--- script.js
+++ script.js
@@ -4,29 +4,40 @@ let firstNumber = "";
 let secondNumber = "";
 let screenValue = "";
 
-let operation;
+let operation = "";
+let previousAnswer = "";
+let answer;
 
-let answer = "";
-
+console.log(screenValue);
 
 function ClearAll() {
   screenValue = "0";
-  answer = "0";
+  firstNumber = "";
+  secondNumber = "";
+  operation = "";
+  answer = "0.00";
+  previousAnswer = "";
+  // Output
   document.getElementById("screen").innerHTML = screenValue;
   document.getElementById("answer").innerHTML = answer;
 }
 
 function ClearScreen(){
   screenValue = "0";
+  firstNumber = "";
+  secondNumber = "";
+  operation = "";
+  answer = Number(previousAnswer);
+  // Output
   document.getElementById("screen").innerHTML= screenValue;
   document.getElementById("answer").innerHTML= answer;
 }
 
 function SevenPressed(){
-  if (screenValue == "0") {
-    screenValue = "7";
-  } else {
+  if (screenValue != "0") {
     screenValue += "7";
+  } else if (screenValue == "0") {
+    screenValue = "7";
   }
 
   document.getElementById('screen').innerHTML= screenValue;
@@ -59,37 +70,87 @@ function ZeroPressed(){
   } else {
     screenValue += "0";
   }
-  
-  
   document.getElementById('screen').innerHTML= screenValue;
+}
+
+function PointPressed(){
+  if (screenValue.includes(".")) {
+    screenValue = screenValue;
+  } else {
+    screenValue += ".";
+  }
+  document.getElementById("screen").innerHTML= screenValue;
 }
 
 
 function PlusPressed(){
-  if (screenValue == "" || screenValue == "0") {
-    screenValue = "0"
+  if (screenValue.includes("+")) {
+    screenValue = screenValue;
   } else {
-    firstNumber = screenValue;
-    operation = "+";
-    screenValue += "+";
+      if (screenValue == "0" || screenValue == ""){
+        screenValue = "0";
+      } else {
+          if (previousAnswer == ""){
+            firstNumber = screenValue;
+            operation = "+";
+            screenValue += "+";
+          }
+          else if(previousAnswer != "" && previousAnswer != "0"){
+            console.log("there's pa");
+            firstNumber = previousAnswer;
+            operation = "+";
+            //
+            screenValue = previousAnswer + "+";
+          } else {
+            screenValue = "Syntax error";
+          }
+      }
   }
+  /////
+  /*else if (screenValue == ""){
+    screenValue = "0";
+  }*/ 
+  /*
+  else {
+    // level 1 else
+      if (screenValue.includes("+")){
+        screenValue = screenValue;
+      } else {
+        // level 2 else
+          
+          else if(previousAnswer != ""){
+            firstNumber = previousAnswer;
+            screenValue = previousAnswer + "+";
+            operation = "+";
+          } else {
+            // level 3 else
+            // cut to first if...
+            screenValue = "Syntax error"
+          }
+      }
+  } */
   
   document.getElementById("screen").innerHTML= screenValue;
 }
 
 function EqualToPressed(){
   if (firstNumber != "" ) {
-    secondNumber = screenValue
+    secondNumber = screenValue.slice(screenValue.indexOf('+') + 1);
+    console.log(typeof(answer));
     switch (operation) {
       case '+':
         // code
         answer = Number(firstNumber) + Number(secondNumber);
+        console.log(secondNumber)
+        console.log(typeof(answer));
+       // answer = result.toString();
         break;
       
     }
   } else {
-    answer = screenValue;
+    answer = Number(screenValue);
   }
   document.getElementById("answer").innerHTML= answer;
+  screenValue = answer.toString();
 }
 
